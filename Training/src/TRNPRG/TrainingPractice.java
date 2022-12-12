04d37a770b93b4e128e4815a058dc0d6f45afd63
Simantika Mishra <SimantikaM@Interrait.com>
2022-12-12 08:08:31 +0530

    Commit from Master

---
+++ Training/src/TRNPRG/TrainingPractice.java
@@ -0,0 +1,10 @@
+package TRNPRG;
+
+public class TrainingPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is Master Branch Content");
+	}
+
+}
